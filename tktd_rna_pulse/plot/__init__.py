d127d8e6358e08ef43c79925c4c448ec731bbb09
flo-schu <fluncki@protonmail.com>
2025-01-31 00:28:57 +0100

    Implement likelihood landscapes as part of the package and add as executable script

---
+++ tktd_rna_pulse/plot/__init__.py
@@ -0,0 +1,11 @@
+from .simulation import (
+    plot_experiment,
+    plot_experiments,
+    plot_variable_substance_combi,
+    pretty_posterior_plot_multisubstance,
+    plot_simulation_results
+)
+
+from .likelihood import (
+    likelihood_landscapes
+)
