3098bad5349a5e15d0864846bdf6825c9ab12022
flo-schu <fluncki@protonmail.com>
2025-02-03 17:56:25 +0100

    Update run lkelihoods to take input from the scenario folder

--- scripts/run_likelihoods.sh
+++ scripts/run_likelihoods.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=0-12:00:00                               # maximum time until job is cancelled
 #SBATCH --ntasks=1                                      # number of tasks
 #SBATCH --cpus-per-task=1                               # number of cpus requested
-#SBATCH --mem-per-cpu=32G                                # memory per cpu requested
+#SBATCH --mem-per-cpu=16G                                # memory per cpu requested
 #SBATCH --mail-type=begin                               # send mail when job begins
 #SBATCH --mail-type=end                                 # send mail when job ends
 #SBATCH --mail-type=fail                                # send mail if job fails
@@ -13,22 +13,15 @@
 
 # this results in n!/(k!(n-k)!) with k = 2 combinations: n=9 --> 36; n=10 --> 45
 # For the pairwise case the equations simplifies to: (n**2-n)/2
-strings=(
-    "k_i_substance"
-    "r_rt_substance"
-    "r_rd_substance"
-    "v_rt_substance"
-    "z_ci_substance"
-    "k_p_substance"
-    "k_m_substance"
-    "z_substance"
-    "kk_substance"
-    # "h_b_substance"
-    # "sigma_nrf2"
-    # "sigma_cint"
-)
-
 config=$1
+
+parameter_file="$(dirname "$config")/parameters_likelihood_landscape.txt"
+
+echo "Processing ${config} ..."
+
+# Read non-comment lines into an array named 'strings'
+mapfile -t strings < <(grep -v '^#' $parameter_file)
+
 parameters=()
 
 # Iterate over the array to create pairs
@@ -51,7 +44,7 @@ index=$SLURM_ARRAY_TASK_ID
 pair=${parameters[index]}
 read -r parx pary <<< "$pair"
 
-echo "Plotting Likelihood Landscapes for $pair ..."
+echo "Plotting Likelihood Landscapes for ${pair} ..."
 
 srun plot-likelihood-landscape \
     --config=${config} \
@@ -59,4 +52,7 @@ srun plot-likelihood-landscape \
     --pary="${pary}" \
     --std_dev=2 \
     --n_grid_points=100 \
-    --n_vector_points=50
+    --n_vector_points=50 \
+    --idata_file="numpyro_posterior.nc" \
+    --sparse \
+    --center-at-posterior-mode
