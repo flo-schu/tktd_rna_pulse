9dbe14b61ae71628401775cf381f207ab2fbbe95
flo-schu <fluncki@protonmail.com>
2025-02-03 14:09:18 +0100

    Include more options in the likelihood landscape computation

--- tktd_rna_pulse/plot/likelihood.py
+++ tktd_rna_pulse/plot/likelihood.py
@@ -8,11 +8,10 @@ from functools import partial
 from frozendict import frozendict
 import jax
 import jax.numpy as jnp
-jax.config.update("jax_enable_x64", True)
 
 from pymob import Config
 
-def likelihood_landscapes(sim, parx, pary, std_dev, n_grid_points, n_vector_points):
+def likelihood_landscapes(sim, parx, pary, std_dev, n_grid_points, n_vector_points, sparse, idata_file, center_at_posterior_mode):
 
     folder = os.path.join(sim.output_path, "likelihood_landscapes")
     os.makedirs(folder, exist_ok=True)
@@ -20,7 +19,6 @@ def likelihood_landscapes(sim, parx, pary, std_dev, n_grid_points, n_vector_poin
     sim.config.inference_numpyro.gaussian_base_distribution = True
     sim.config.jaxsolver.throw_exception = False
     sim.config.jaxsolver.max_steps = 10_000
-    sim.setup()
 
     # set up the bounds for likelihood landscapes
     # this must be done here
@@ -31,45 +29,65 @@ def likelihood_landscapes(sim, parx, pary, std_dev, n_grid_points, n_vector_poin
 
     sim.dispatch_constructor()
     sim.set_inferer("numpyro")
-    sim.inferer.load_results()
+    sim.inferer.load_results(idata_file)
+    
+    if hasattr(sim.inferer.idata, "posterior"):
+        group = "posterior"
+    elif hasattr(sim.inferer.idata, "prior"):
+        group = "prior"
+    else:
+        raise AttributeError("idata has neither prior nor posterior.")
 
     def dataset_to_dict(dataset: xr.Dataset):
         return {k: v.values for k, v in dataset.data_vars.items()}
     
     mode_draw = sim.inferer.idata.log_likelihood.sum(("id", "time")).to_array().sum("variable").argmax()
     
-    mode = dataset_to_dict(sim.inferer.idata.unconstrained_posterior.sel(chain=0, draw=mode_draw))
+    mode = dataset_to_dict(sim.inferer.idata[f"unconstrained_{group}"].sel(chain=0, draw=mode_draw))
     mpx = np.array(mode[f"{parx}_normal_base"], ndmin=1)
     mpy = np.array(mode[f"{pary}_normal_base"], ndmin=1)
     mode_frozen = frozendict({k: tuple(np.array(v, ndmin=1).tolist()) for k, v in mode.items()})
 
     # get extra dim
-    parx_extra_dim = [d for d in sim.inferer.idata.posterior[parx].dims if d not in ("chain", "draw")]
+    parx_extra_dim = [d for d in sim.inferer.idata[group][parx].dims if d not in ("chain", "draw")]
     if len(parx_extra_dim) == 0:
         parx_coords = [""]
         parx_extra_dim = ""
     elif len(parx_extra_dim) == 1:
-        parx_coords = [f" [{v}]" for v in sim.inferer.idata.posterior[parx].coords[parx_extra_dim[0]].values]
+        parx_coords = [f" [{v}]" for v in sim.inferer.idata[group][parx].coords[parx_extra_dim[0]].values]
         parx_extra_dim = parx_extra_dim[0]
     else:
         raise ValueError("Only 1-D Parameters are supported")
 
     # get extra dim
-    pary_extra_dim = [d for d in sim.inferer.idata.posterior[pary].dims if d not in ("chain", "draw")]
+    pary_extra_dim = [d for d in sim.inferer.idata[group][pary].dims if d not in ("chain", "draw")]
     if len(pary_extra_dim) == 0:
         pary_coords = [""]
         pary_extra_dim = ""
     elif len(pary_extra_dim) == 1:
-        pary_coords = [f" [{v}]" for v in sim.inferer.idata.posterior[pary].coords[pary_extra_dim[0]].values]
+        pary_coords = [f" [{v}]" for v in sim.inferer.idata[group][pary].coords[pary_extra_dim[0]].values]
         pary_extra_dim = pary_extra_dim[0]
     else:
         raise ValueError("Only 1-D Parameters are supported")
 
+    if sparse:
+        nrows = 1
+    else:
+        nrows = len(mpy)
 
-    fig, axes = plt.subplots(ncols=len(mpx), nrows=len(mpy), figsize=(14,10), squeeze=False)
+    fig, axes = plt.subplots(ncols=len(mpx), nrows=nrows, figsize=(2+len(mpx)*4,2+nrows*3), squeeze=False)
 
     for i, (axcol, x) in enumerate(zip(axes.T, mpx)):
-        for j, (ax, y) in enumerate(zip(axcol, mpy)):
+        for j, y in enumerate(mpy):
+            if sparse:
+                if i != j:
+                    # skip the execution of combinations that are not on the diagonal
+                    continue
+                else:
+                    ax = axcol[0]
+            else:
+                ax = axcol[j]
+
             sim.dispatch_constructor()
             sim.set_inferer("numpyro")
 
@@ -109,10 +127,14 @@ def likelihood_landscapes(sim, parx, pary, std_dev, n_grid_points, n_vector_poin
             grad = jax.grad(partial(func, _i=i, _j=j, _mode=mode_frozen, _parx=parx, _pary=pary))
             func_ = partial(func, _i=i, _j=j, _mode=mode_frozen, _parx=parx, _pary=pary)
 
+            if center_at_posterior_mode:
+                bounds=([x - std_dev, x + std_dev], [y - std_dev, y + std_dev])
+            else:
+                bounds=([0 - std_dev, 0 + std_dev], [0 - std_dev, 0 + std_dev])
+            
             ax = sim.inferer.plot_likelihood_landscape(
                 parameters=(parx, pary),
-                bounds=([x - std_dev, x + std_dev], [y - std_dev, y + std_dev]),
-                # bounds=([0 - std_dev, 0 + std_dev], [0 - std_dev, 0 + std_dev]),
+                bounds=bounds,
                 log_likelihood_func=func_,
                 gradient_func=None if n_vector_points== 0 else grad,
                 n_grid_points=n_grid_points,
@@ -135,7 +157,10 @@ def likelihood_landscapes(sim, parx, pary, std_dev, n_grid_points, n_vector_poin
 @click.option("--n_grid_points", type=int, default=50)
 @click.option("--n_vector_points", type=int, default=0)
 @click.option("--debug/--no-debug", default=False)
-def main(config, parx, pary, std_dev, n_grid_points, n_vector_points, debug):
+@click.option("--sparse/--full", default=False)
+@click.option("--idata_file", default=None)
+@click.option("--center-at-posterior-mode/--center-at-prior-mode", default=False)
+def main(config, parx, pary, std_dev, n_grid_points, n_vector_points, debug, sparse, idata_file, center_at_posterior_mode):
 
     if debug:
         import pdb
@@ -145,6 +170,7 @@ def main(config, parx, pary, std_dev, n_grid_points, n_vector_points, debug):
     cfg.import_casestudy_modules()
     Simulation = cfg.import_simulation_from_case_study()
     sim = Simulation(config)
+    sim.setup()
 
     likelihood_landscapes(
         sim=sim,
@@ -152,7 +178,10 @@ def main(config, parx, pary, std_dev, n_grid_points, n_vector_points, debug):
         pary=pary,
         std_dev=std_dev,
         n_grid_points=n_grid_points,
-        n_vector_points=n_vector_points
+        n_vector_points=n_vector_points,
+        sparse=sparse,
+        idata_file=idata_file,
+        center_at_posterior_mode=center_at_posterior_mode
     )
 
 
@@ -168,7 +197,8 @@ if __name__ == "__main__":
             "--pary=r_rt_substance",
             "--n_grid_points=5",
             "--n_vector_points=5",
-            "--no-debug"
+            "--no-debug",
+            "--full"
         ])
         if isinstance(result.exception, SystemExit):
             raise KeyError(
